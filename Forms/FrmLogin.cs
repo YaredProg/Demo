8c0d6c3bba6ccc48a2cc06a6c4882a39c5868ebd
YaredProg <yaayared@gmail.com>
2024-06-08 04:25:56 +0200

    Solve proplem in code

--- Forms/FrmLogin.cs
+++ Forms/FrmLogin.cs
@@ -8,11 +8,8 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
-<<<<<<< HEAD
 using System.Threading.Tasks;
-=======
 using System.Linq;
->>>>>>> test
 using System.Windows.Forms;
 
 namespace BookProject.Forms
